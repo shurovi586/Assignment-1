ff0b0258abe0fb697c73384cd8c7399c75bd0436
Nusrat Sunzida Shurovi <139633555+shurovi586@users.noreply.github.com>
2024-07-07 12:01:19 -0600

    Add files via upload

---
+++ Assignment-1/celsius_to_fahrenheit.php
@@ -0,0 +1,8 @@
+<?php
+$temperature_value = 32;
+$fahrenheit = (((9*$temperature_value)/5) + 32);
+
+printf("Temperature is %.2f degree fahrenheit",$fahrenheit);
+
+
+
---
+++ Assignment-1/largest_number.php
@@ -0,0 +1,22 @@
+<?php
+
+$num1 = 4;
+$num2 = 5;
+$num3 = 6;
+
+if ($num1 > $num2 && $num1 > $num3)
+ {
+    echo"num1 = $num1 is the largest number";
+ }
+
+else if ($num2 > $num1 && $num2 > $num3)
+ {
+    echo "num2 = $num2 is the largest number";
+ }
+
+else
+ {
+    echo "num3 = $num3 is the largest number";
+ }
+ 
+
